18b60d07dec48651a64c9d6c6f1737256263e36c
Sebastian Gogoasa <sebastian.gogoasa@cloudresty.com>
2025-04-16 00:02:06 +0100

    Archive path updated

--- ulid.rb
+++ ulid.rb
@@ -1,17 +1,18 @@
 class Ulid < Formula
   desc "ULID (Universally Unique Lexicographically Sortable Identifier) generator"
   homepage "https://github.com/cloudresty/goulid"
-  url "https://github.com/cloudresty/goulid/archive/${GITHUB_REF_NAME}.tar.gz"
+  url "https://github.com/cloudresty/goulid/archive/refs/tags/${GITHUB_REF_NAME}.tar.gz"
   sha256 "YOUR_SHA256_CHECKSUM"
 
   depends_on "go" => :build
 
   def install
-    ldflags = "-X main.version=${GITHUB_REF_NAME}"
+    ldflags = "-X main.version=${GITHUB_REF_NAME} -X main.repoURL=https://github.com/cloudresty/goulid"
     system "go", "build", "-ldflags", ldflags, "-o", bin/"ulid", "./cmd/ulid/main.go"
   end
 
   test do
     assert_match "${GITHUB_REF_NAME}", shell_output("#{bin}/ulid --version").strip
+    assert_match "https://github.com/cloudresty/goulid", shell_output("#{bin}/ulid --version")
   end
 end
